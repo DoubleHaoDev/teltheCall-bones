6ff14e037bee652d57963d787bd6c5b4dd7470c8
Andy Chang <andychang@Andys-Laptop-2.hitronhub.home>
2023-12-18 20:39:53 -0800

    Apply formatting to every existing classes

--- src/main/java/com/teltheCall/bones/controller/HelloWorld.java
+++ src/main/java/com/teltheCall/bones/controller/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.teltheCall.bones.controller;
 
-
 import com.teltheCall.bones.model.TestTable;
 import com.teltheCall.bones.service.TestTableService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,27 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/comment")
 public class HelloWorld {
 
-    private TestTableService testTableService;
+  private final TestTableService testTableService;
 
-    @Autowired
-    public HelloWorld(TestTableService testTableService) {
-        this.testTableService = testTableService;
+  @Autowired
+  public HelloWorld(TestTableService testTableService) {
+    this.testTableService = testTableService;
+  }
+
+  @GetMapping("/hello")
+  public String helloMethod() {
+
+    return "Service is up";
+  }
+
+  @GetMapping("/testTable/{testTableId}")
+  public ResponseEntity<TestTable> getTestTableById(@PathVariable long testTableId) {
+
+    TestTable testTable = testTableService.findById(testTableId);
+    if (testTable == null) {
+      return ResponseEntity.notFound().build();
     }
-
-    @GetMapping("/hello")
-    public String helloMethod(){
-
-        return "Service is up";
-    }
-
-    @GetMapping("/testTable/{testTableId}")
-    public ResponseEntity<TestTable> getTestTableById(@PathVariable long testTableId){
-
-        TestTable testTable = testTableService.findById(testTableId);
-        if(testTable == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(testTable);
-    }
+    return ResponseEntity.ok(testTable);
+  }
 
 }
--- src/test/java/com/teltheCall/bones/BonesApplicationTests.java
+++ src/test/java/com/teltheCall/bones/BonesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BonesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
